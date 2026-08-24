03d9940d4bdcde489b322619f107ac4a072ea8ab
aloneWalker <email_zx@163.com>
2026-08-24 09:16:29 +0800

    提交板载按键控制板载LED实验代码

--- stm32-f407-experiments/02-KEY/User/main.c
+++ stm32-f407-experiments/02-KEY/User/main.c
@@ -1,17 +1,36 @@
 #include "./SYSTEM/sys/sys.h"
 #include "./SYSTEM/delay/delay.h"
 #include "./SYSTEM/usart/usart.h"
+#include "./BSP/LED/led-i.h"
+#include "./BSP/KEY/key-i.h"
 
 int main(void)
-{
+{   
+    uint8_t key;
     HAL_Init();                         /* 初始化HAL库 */
     sys_stm32_clock_init(336, 8, 2, 7); /* 配置时钟，168MHz */
     delay_init(168);                    /* 初始化延时 */
     usart_init(115200);                 /* 初始化串口 */
-    
+    led_i_init();                         /* 初始化LED */
+    key_i_init();                         /* 初始化按键 */
     while (1)
     {
-        printf("Hello, M144Z-M4 STM32F407!\r\n");
-        delay_ms(1000);
+        key = key_i_scan(0);              /* 扫描按键 */
+        
+        switch (key)
+        {
+            case WKUP_PRESS:             /* WKUP按键被按下 */
+            {
+                LED0_TOGGLE();          /* 翻转LED0状态 */
+                break;
+            }
+            case KEY0_PRESS:             /* KEY0按键被按下 */
+            {
+                LED1_TOGGLE();          /* 翻转LED1状态 */
+                break;
+            }
+        }
+        
+        delay_ms(10);
     }
 }
